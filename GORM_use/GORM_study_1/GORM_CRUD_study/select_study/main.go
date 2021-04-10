c42bcaad4df2b2595cf11eb5bc7b3ff54d38cbb7
wangzeshen-sys <wzs809036592@163.com>
2021-04-09 19:04:50 -0700

    add

--- GORM_use/GORM_study_1/GORM_CRUD_study/select_study/main.go
+++ GORM_use/GORM_study_1/GORM_CRUD_study/select_study/main.go
@@ -45,7 +45,8 @@ func main() {
 	var u3 []UserInfo // 定义一个切片类型
 	// 查询所有记录
 	// select * from user_infos;
-	Db.Debug().Find(&u3)
+	// Db.Debug().Find(&u3)
+	Db.Find(&u3)
 	fmt.Println("表中的所有记录")
 	fmt.Println(u3)
 
@@ -69,20 +70,115 @@ func main() {
 	// IN
 	fmt.Println("IN TEST")
 	Db.Where("name IN (?)", []string{"小刚", "小丽"}).Find(&u3)
+	// select * from user_infos where name in ("小刚", "小丽");
 	fmt.Println(u3)
 
 	// LIKE
 	fmt.Println("LIKE TEST")
 	Db.Where("name LIKE ?", "%丽%").Find(&u3)
+	// select * from user_infos where name like "%丽%";
 	fmt.Println(u3)
 	
 	// AND
 	fmt.Println("AND TEST")
 	Db.Where("name=? AND gender=?", "小丽", "女").Find(&u3)
+	// select * from user_infos where name="小丽" and gender="女";
 	fmt.Println(u3)
 
-	//Time
-	Db.Where("UpdatedAt > ?", 3).Find(&u3)
-	Db.Where("CreatedAt BETWEEN ? AND ?", 3, 8).Find(&u3)
+	//Time  目前没有测试效果
+	// Db.Where("UpdatedAt > ?", 3).Find(&u3)
+	// Db.Where("CreatedAt BETWEEN ? AND ?", 3, 8).Find(&u3)
+
+
+	// where 查询（struct map）
+	// struct
+	var u5 UserInfo
+	Db.Where(&UserInfo{Name: "小刚", Gender: "女"}).First(&u5)
+	// select * from user_infos where name="小刚" and gender="女" limit 1;
+	fmt.Println(u5)
+	// map
+	Db.Where(map[string]interface{}{"name":"小刚", "gender":"女"}).Find(&u3)
+	// select * from user_infos where name="小刚" and gender="女";
+	fmt.Println(u3)
+
+	// not 
+	fmt.Println("Not")
+	Db.Not("name", "小丽").Find(&u3)
+	// select * from user_infos where name <> "小丽";
+	fmt.Println(u3)
+	
+	Db.Not("name", []string{"小明", "小明"}).Find(&u3)
+	// select * from user_infos where name not in ("小明","小明");
+	fmt.Println(u3)
+
+	var u6 UserInfo
+	Db.Not([]int64{1,2,3}).First(&u6)
+	// select * from user_infos where id not  in (1,2,3);
+	fmt.Println(u6)
+
+	var u7 []UserInfo
+	Db.Not([]int64{}).Find(&u7)
+	// select * from user_infos;
+	fmt.Println("u7:", u7)
+
+	var u8 []UserInfo
+	// 下面的SQL 不知道为什么没有查到数据
+	// Db.Not("name=?","小刚").Find(&u8)
+	// fmt.Println(u8)
+	Db.Not(UserInfo{Name: "小丽"}).Find(&u8)
+	// select * from user_infos where name <> '小丽';
+	fmt.Println("u8", u8)
+
+	// 带内联条件的查询
+	fmt.Println("=============================================")
+	fmt.Println("带内联条件的查询")
+	var u9 UserInfo
+	Db.First(&u9, 2)
+	// select * from user_infos where id=2 limit 1;
+	fmt.Println("u9", u9)
+
+	var u10 UserInfo
+	Db.Find(&u10, "name=?", "小丽")
+	// select * from user_infos where name='小丽';
+	fmt.Println("u10", u10)
+	Db.Find(&u3, "name <> ? and gender <> ?", "小刚", "女")
+	// select * from user_infos where name <>'小刚 and gender<>'女';
+	fmt.Println("u3", u3)
+
+	// struct 
+	Db.Find(&u3, UserInfo{Gender: "女"})
+	// select * from user_infos where gender='女';
+	fmt.Println("u3",u3)
+	// map
+	Db.Find(&u3, map[string]interface{}{"gender":"女"})
+	// selct * from user_infos where gender='女';
+	fmt.Println("u3", u3)
+
+	// or条件查询
+	fmt.Println("or 条件查询")
+	Db.Where("name=?", "小丽").Or("name=?", "小刚").Find(&u3)
+	// select * from user _infos where name='小丽' or name='小刚';
+	fmt.Println("u3", u3)
+	// struct
+	Db.Where("name=?", "小智").Or(UserInfo{Name: "小明"}).Find(&u3)
+	// selct * from user_infos where name='小明' or name='小明';
+	fmt.Println("u3", u3)
+
+	// 查询链
+	fmt.Println("查询链")
+	Db.Debug().Where("name <> ?", "小刚").Where("gender=?", "女").Find(&u3)
+	// SELECT * FROM `user_infos`  WHERE (name <> '小刚') AND (gender='女')
+	fmt.Println("u3", u3)
+
+	
+
+
+
+
+
+
+
+
+
 
 }
